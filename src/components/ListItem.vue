7d1904260a19b16260735cfcc1754bdaa21bf837
Ranieli <ranielisilveira@hotmail.com>
2022-07-01 14:23:01 -0300

    Create ListItem

---
+++ src/components/ListItem.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <b-card :title="seed.subject" class="mb-2">
+    <b-card-text>{{ seed.description }}</b-card-text>
+
+    <b-button variant="outline-secondary" class="mr-2" @click="edit(seed)">
+      Editar
+    </b-button>
+    <b-button variant="outline-danger" class="mr-2" @click="remove(seed)">
+      Excluir
+    </b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    seed: {
+      type: Object,
+      required: true,
+      default: () => ({})
+    },
+  },
+  methods: {
+    remove(seed) {
+      this.$emit('remove', seed);
+    },
+    edit(seed) {
+      this.$emit('edit', seed);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
